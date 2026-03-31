04951a29acdbb72ac0751f534842df6407824909
mohd.mukarram <mukarram.techie@gmail.com>
2026-03-31 23:19:50 +0530

    Remove node_modules and add gitignore

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Node.js CI/CD Pipeline Working!");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+module.exports = app;

5670d6bf1cf0dee98ac4a890a49ae20366cb511d
mohd.mukarram <mukarram.techie@gmail.com>
2026-04-02 00:28:40 +0530

    live app

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,5 @@
 resource "aws_vpc" "main" {
+
   cidr_block = "10.0.0.0/16"
 
   tags = {
@@ -6,17 +7,56 @@ resource "aws_vpc" "main" {
   }
 }
 
-resource "aws_subnet" "main_subnet" {
+
+resource "aws_internet_gateway" "igw" {
 
   vpc_id = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
 
+  tags = {
+    Name = "${var.environment}-igw"
+  }
+
+}
+
+
+resource "aws_subnet" "main_subnet" {
+
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
 
   tags = {
     Name = "${var.environment}-subnet"
   }
+
 }
+
+
+resource "aws_route_table" "public_rt" {
+
+  vpc_id = aws_vpc.main.id
+
+  route {
+
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+
+  }
+
+  tags = {
+    Name = "${var.environment}-public-rt"
+  }
+
+}
+
+
+resource "aws_route_table_association" "public_assoc" {
+
+  subnet_id      = aws_subnet.main_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+
+}
+
 
 resource "aws_security_group" "node_sg" {
 
@@ -24,46 +64,62 @@ resource "aws_security_group" "node_sg" {
   vpc_id = aws_vpc.main.id
 
   ingress {
+
     from_port   = 3000
     to_port     = 3000
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+
   }
 
   ingress {
+
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+
   }
 
   egress {
+
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+
   }
 
   tags = {
     Name = "${var.environment}-node-sg"
   }
+
 }
+
 
 resource "aws_iam_role" "ec2_role" {
 
   name = "${var.environment}-ec2-role"
 
   assume_role_policy = jsonencode({
+
     Version = "2012-10-17"
+
     Statement = [{
+
       Action = "sts:AssumeRole"
       Effect = "Allow"
+
       Principal = {
         Service = "ec2.amazonaws.com"
       }
+
     }]
+
   })
+
 }
+
 
 resource "aws_iam_role_policy_attachment" "ec2_policy" {
 
@@ -72,12 +128,14 @@ resource "aws_iam_role_policy_attachment" "ec2_policy" {
 
 }
 
+
 resource "aws_iam_instance_profile" "ec2_profile" {
 
   name = "${var.environment}-ec2-profile"
   role = aws_iam_role.ec2_role.name
 
 }
+
 
 resource "aws_instance" "node_server" {
 
@@ -92,17 +150,39 @@ resource "aws_instance" "node_server" {
 
   iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
 
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+
+              # Install NodeJS
+              curl -sL https://rpm.nodesource.com/setup_18.x | bash -
+              yum install -y nodejs git
+
+              # Clone your repository
+              cd /home/ec2-user
+              git clone https://github.com/MukarramRepository/nodejs-cicd.git
+
+              cd nodejs-cicd
+
+              npm install
+
+              # Start application
+              nohup node app.js > app.log 2>&1 &
+              EOF
+
   tags = {
     Name = "${var.environment}-node-app"
   }
 
 }
 
+
 resource "aws_secretsmanager_secret" "node_secret" {
 
-  name = "nodejs-app-secret"
+  name = "${var.environment}-nodejs-app-secret"
 
 }
+
 
 resource "aws_secretsmanager_secret_version" "secret_value" {
 
